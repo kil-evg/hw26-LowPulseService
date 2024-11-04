aaabc2123fa59aace599012476f6a75156ecf0c3
EKil <loreleyko@gmail.com>
2024-11-04 01:15:17 +0100

    Homework26

--- src/main/java/ait/cohort46/pulse/service/PulseService.java
+++ src/main/java/ait/cohort46/pulse/service/PulseService.java
@@ -2,17 +2,24 @@ package ait.cohort46.pulse.service;
 
 
 import ait.cohort46.pulse.dto.PulseDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import java.util.function.Consumer;
 @Service
 public class PulseService {
+    @Value("${pulse.min}")
+    private int minPulse;
+
     @Bean
     public Consumer<PulseDto> receiveData() {
-        return s -> {
-            long delay = System.currentTimeMillis() - s.getTimestamp();
-            System.out.println("delay: " + delay + ", id: " + s.getId() + ", pulse " + s.getPayload());
+        return p -> {
+            if (p.getPayload() < minPulse) {
+                long delay = System.currentTimeMillis() - p.getTimestamp();
+                System.out.println("delay: " + delay + ", id: " + p.getId() + ", pulse " + p.getPayload());
+            }
         };
     }
 }
+
